c7d344911838618b6a092342b64e9fd2fa4a0c38
naye2m <naye2m@outlook.com>
2024-10-11 08:26:19 +0600

    Creating shell file for running programms one click

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Please provide a mode"
+    echo "Usage: $0 <mode>[vitedev|vitebuild]"
+    exit 1
+fi
+
+if [ "$1" == "vitedev" ]; then
+    echo "Running server"
+    cd FPSGameFrontend/ && npx vite dev --port 5500
+    elif [ "$1" == "vitebuild" ]; then
+    echo "Running build"
+    cd FPSGameFrontend/ && npx build -e
+    if [ $? -ne 0 ]; then
+        echo "Build failed"
+        exit 1
+    fi
+    echo "Build successful"
+    if [ "$2" == "preview" ]; then
+        echo "Running preview"
+        npx vite preview --port 5500
+    fi
+else
+    echo "<mode>[$1] is not in list [vitedev|vitebuild]"
+    exit 1
+fi
